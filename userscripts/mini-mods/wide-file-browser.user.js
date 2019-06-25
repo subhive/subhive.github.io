feb5615ec9ced9239a1d9c7f0077dc9e4b17aa9a
subhive <40172542+subhive@users.noreply.github.com>
2019-06-25 17:17:12 +1000

    Create wide-file-browser.user.js

---
+++ userscripts/mini-mods/wide-file-browser.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         [Canvas Tweaks] Wide file browser
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Make the file browser wider when editing pages.
+// @author       You
+// @match        https://*.instructure.com/courses/*/*/*/edit
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Edit styles
+    GM_addStyle('#right-side-wrapper {width: 400px;}');
+    GM_addStyle('._mtenas {font-size: .7em;}');
+
+})();
