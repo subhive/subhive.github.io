704814cac69cea2984461840429027654585aa58
subhive <40172542+subhive@users.noreply.github.com>
2019-07-25 10:41:36 +1000

    Update

--- userscripts/mini-mods/wide-file-browser.user.js
+++ userscripts/mini-mods/wide-file-browser.user.js
@@ -1,18 +1,22 @@
 // ==UserScript==
 // @name         [Canvas mini-mod] Wide file browser
-// @description  Make the file browser wider when editing pages and assignments
+// @description  Make the file browser wider when editing pages.
 // @version      1.0
 // @namespace    https://subhive.github.io
 // @author       kebbott@swin.edu.au
 // @match        https://*.instructure.com/courses/*/*/*/edit
+// @match        https://*.instructure.com/courses/*/files/*
 // @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Edit styles
+    // In-page file browser
     GM_addStyle('#right-side-wrapper {width: 400px;}');
     GM_addStyle('._mtenas {font-size: .7em;}');
+
+    // Files file browser
+    GM_addStyle('.ef-name-col {flex: 17;}');
 
 })();
